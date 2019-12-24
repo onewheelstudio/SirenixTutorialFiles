12ba712905006d585b973062135dbddf3a0153e2
onewheelstudio <jeremytylerwolf@gmail.com>
2019-12-23 21:29:39 -0700

    Added SOs with Odin
    Added files for "SOs with Odin"

---
+++ ScriptableObjectsWithOdin/Editor/EnemyDataEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+
+public class EnemyDataEditor : OdinMenuEditorWindow
+{
+    [MenuItem("Tools/Enemy Data")]
+    private static void OpenWindow() 
+    {
+        GetWindow<EnemyDataEditor>().Show();
+    }
+
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        var tree = new OdinMenuTree();
+        tree.Selection.SupportsMultiSelect = false;
+
+        tree.Add("Create New", new CreateNewEnemyData());
+        tree.AddAllAssetsAtPath("Enemy Data", "Assets/Scripts", typeof(EnemyData));
+        return tree;
+    }
+
+    public class CreateNewEnemyData
+    {
+        public CreateNewEnemyData()
+        {
+            enemyData = ScriptableObject.CreateInstance<EnemyData>();
+            enemyData.enemyName = "New Enemy Data";
+        }
+
+        [InlineEditor(Expanded = true)]
+        public EnemyData enemyData;
+
+        [Button("Add New Enemy SO")]
+        private void CreateNewData()
+        {
+            AssetDatabase.CreateAsset(enemyData, "Assets/Scripts/" + enemyData.enemyName + ".asset");
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+    protected override void OnBeginDrawEditors()
+    {
+        OdinMenuTreeSelection selected = this.MenuTree.Selection;
+
+        SirenixEditorGUI.BeginHorizontalToolbar();
+        {
+            GUILayout.FlexibleSpace();
+
+            if (SirenixEditorGUI.ToolbarButton("Delete Current"))
+            {
+                EnemyData asset = selected.SelectedValue as EnemyData;
+                string path = AssetDatabase.GetAssetPath(asset);
+                AssetDatabase.DeleteAsset(path);
+                AssetDatabase.SaveAssets();
+            }
+
+        }
+        SirenixEditorGUI.EndHorizontalToolbar(); 
+    }
+}
---
+++ ScriptableObjectsWithOdin/EnemyData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "My Game/Enemy Data")]
+[InlineEditor]
+public class EnemyData : ScriptableObject
+{
+    [BoxGroup("Basic Info")]
+    [LabelWidth(100)]
+    public string enemyName;
+    [BoxGroup("Basic Info")]
+    [LabelWidth(100)]
+    [TextArea]
+    public string description;
+
+    [HorizontalGroup("Game Data", 75)]
+    [PreviewField(75)]
+    [HideLabel]
+    public GameObject enemyModel;
+
+    [VerticalGroup("Game Data/Stats")]
+    [LabelWidth(100)]
+    [Range(20,100)]
+    [GUIColor(0.5f,1f,0.5f)]
+    public int health = 20;
+    [VerticalGroup("Game Data/Stats")]
+    [LabelWidth(100)]
+    [Range(0.5f,5f)]
+    [GUIColor(0.3f,0.5f,1f)]
+    public float speed = 2f;
+    [VerticalGroup("Game Data/Stats")]
+    [LabelWidth(100)]
+    [Range(5,30)]
+    [GUIColor(1f,1f,0f)]
+    public float detectRange = 10f;
+    [VerticalGroup("Game Data/Stats")]
+    [LabelWidth(100)]
+    [Range(1,10)]
+    [GUIColor(0.8f,0.4f,0.4f)]
+    public int damage = 1;
+}
+
+
