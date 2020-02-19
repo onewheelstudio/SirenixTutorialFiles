531f7909d72bc9ac5ef31ea0ff03265573a539ca
onewheelstudio <jeremytylerwolf@gmail.com>
2020-02-19 07:58:57 -0700

    Added Property Processors Files
    Added files from the property processor video: https://youtu.be/Z5Pyzc_VZ54

---
+++ PropertyProcessors/SampleClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleClass : MonoBehaviour
+{
+    public string top;
+    public string middle;
+    public string bottom;
+
+    public int value1 = 1;
+    public int value2 = 2;
+    public int value3 = 3;
+
+}
+
+public enum MyEnum
+{
+    zero,
+    one,
+    two,
+    three,
+    four
+}
---
+++ PropertyProcessors/SamplePropertyProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using System;
+
+public class SamplePropertyProcessor<T> : OdinPropertyProcessor<T> where T : SampleClass
+{
+    public override void ProcessMemberProperties(List<InspectorPropertyInfo> propertyInfos)
+    {
+        for (int i = 0; i < propertyInfos.Count; i++)
+        {
+            if(propertyInfos[i].PropertyName == "bottom")
+            {
+                propertyInfos.Insert(0, propertyInfos[i]);
+                propertyInfos.RemoveAt(i + 1);
+            }
+        }
+
+        propertyInfos.AddDelegate("Print Hello", () => Debug.Log("Hello"), new BoxGroupAttribute("injected"));
+
+        propertyInfos.AddValue("Injected Property",
+            (ref SampleClass s) => s.value1 + s.value2 + s.value3,
+            (ref SampleClass s, int sum) => { }, new BoxGroupAttribute("injected"));
+
+        propertyInfos.AddValue("Injected Enum",
+            (ref SampleClass s) => (MyEnum)s.value1,
+            (ref SampleClass s, MyEnum myEnum) => s.value1 = (int)myEnum,
+            new EnumToggleButtonsAttribute(),
+            new BoxGroupAttribute("injected"));
+
+        propertyInfos.Remove("value1");
+    }
+}
