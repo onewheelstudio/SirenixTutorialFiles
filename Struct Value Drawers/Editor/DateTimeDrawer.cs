165274df5d27808b25e82ca331808bab355bda57
onewheelstudio <jeremytylerwolf@gmail.com>
2020-03-11 07:04:23 -0600

    Struct Value Drawer Added

---
+++ Struct Value Drawers/Editor/DateTimeDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.Utilities.Editor;
+using Sirenix.OdinInspector.Editor;
+using System;
+
+public class DateTimeDrawer : OdinValueDrawer<DateTime>
+{
+    private string display;
+    private object key = new object();
+
+    protected override void DrawPropertyLayout(GUIContent label)
+    {
+        display = this.ValueEntry.SmartValue.ToString();
+
+        SirenixEditorGUI.BeginShakeableGroup(key);
+
+        display = SirenixEditorFields.TextField(label, display);
+
+        if(DateTime.TryParse(display, out DateTime result))
+        {
+            this.ValueEntry.SmartValue = result;
+        }
+        else
+        {
+            SirenixEditorGUI.StartShakingGroup(key);
+        }
+
+        SirenixEditorGUI.EndShakeableGroup(key);
+    }
+}
---
+++ Struct Value Drawers/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
+
+public class MyClass : MonoBehaviour
+{
+    [ShowInInspector]
+    public DateTime myDateTime;
+}
