9e327eb9391fbbbcbe32939f9bc1404b3cd8df26
onewheelstudio <jeremytylerwolf@gmail.com>
2022-01-24 07:29:49 -0700

    Added Data Manager Files

---
+++ Data Manager/DataManager.cs
@@ -0,0 +1,40 @@
+﻿    using Sirenix.OdinInspector.Editor;
+    using System;
+    using System.Linq;
+    using UnityEditor;
+
+    public class DataManager : OdinMenuEditorWindow
+    {
+        private static Type[] typesToDisplay = TypeCache.GetTypesWithAttribute<ManageableDataAttribute>()
+            .OrderBy(m => m.Name)
+            .ToArray();
+
+        private Type selectedType;
+
+        [MenuItem("Tools/Data Manager")]
+        private static void OpenEditor() => GetWindow<DataManager>();
+
+        //protected override void OnGUI()
+        //{
+        //    //draw menu tree for SOs and other assets
+        //    if (GUIUtils.SelectButtonList(ref selectedType, typesToDisplay))
+        //        this.ForceMenuTreeRebuild();
+
+        //    base.OnGUI();
+        //}
+
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+        foreach (var type in typesToDisplay)
+        {
+            tree.AddAllAssetsAtPath(type.Name, "Assets/", type, true, true);
+
+
+        }
+            return tree;
+        }
+    }
+
+
+
---
+++ Data Manager/GUIUtils.cs
@@ -0,0 +1,48 @@
+        using Sirenix.Utilities;
+        using System;
+        using UnityEditor;
+        using UnityEngine;
+        using Sirenix.Utilities.Editor;
+
+        public static class GUIUtils
+        {
+            public static bool SelectButtonList(ref Type selectedType, Type[] typesToDisplay)
+            {
+                var rect = GUILayoutUtility.GetRect(0, 25);
+
+                for (int i = 0; i < typesToDisplay.Length; i++)
+                {
+                    var name = typesToDisplay[i].Name;
+                    var btnRect = rect.Split(i, typesToDisplay.Length);
+
+                    if (GUIUtils.SelectButton(btnRect, name, typesToDisplay[i] == selectedType))
+                    {
+                        selectedType = typesToDisplay[i];
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public static bool SelectButton(Rect rect, string name, bool selected)
+            {
+                if (GUI.Button(rect, GUIContent.none, GUIStyle.none))
+                    return true;
+
+                if (Event.current.type == EventType.Repaint)
+                {
+                    var style = new GUIStyle(EditorStyles.miniButtonMid);
+                    style.stretchHeight = true;
+                    style.fixedHeight = rect.height;
+                    style.Draw(rect, GUIHelper.TempContent(name), false, false, selected, false);
+                }
+
+
+                return false;
+            }
+
+        } 
+
+
+
+
